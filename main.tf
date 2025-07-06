2f6caf378b35ace9f26bdbd2161f7d66bdee1b59
unknown <brijendrayadav014@gmail.com>
2025-07-06 22:57:59 +0530

    Storage account add

--- main.tf
+++ main.tf
@@ -2,3 +2,14 @@ resource "azurerm_resource_group" "rg1" {
   name     = "example-resources"
   location = "West Europe"
 }
+resource "azurerm_storage_account" "storage1" {
+  name                     = "examplestorageaccount"
+  resource_group_name      = azurerm_resource_group.rg1.name
+  location                 = azurerm_resource_group.rg1.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
